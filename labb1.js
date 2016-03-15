34cd3ea26ed8774caf1fcf2d9be39b2590ba1411
Tobias Årud <me@tobiasarud.se>
2016-03-15 19:41:48 +0100

    Tuesday code
    solution for cross imminent...

--- labb1.js
+++ labb1.js
@@ -10,6 +10,12 @@ function makeRowsAndColumns(){
         }
     }
     
+    for(var i = 0; i < 5; i++){
+        for(var j = 0; j < 5; j++){
+            table.rows[i].cells[j].id = "y" + i + "x" + j;
+        }
+    }
+    
     var arr = document.querySelectorAll("td");
     for(var i = 0; i < arr.length; i++){
         arr[i].addEventListener("click", colorChange);
@@ -19,12 +25,27 @@ function makeRowsAndColumns(){
 makeRowsAndColumns();
 
 function colorChange(){
+    table.rows.classList.remove("empty");
+    table.rows.classList.add("filled");
+}
+
+/*
+function changeContent(id, row, cell, content) {
+    var x = document.getElementById(id).rows[row].cells;
+    x[cell].innerHTML = content;
+}
+*/
+
+    /* Good code for changing one cell
+    var td = document.getElementsByTagName("td");
     if(this.classList.contains("empty")){
         this.classList.remove("empty");
         this.classList.add("filled");
+        this.previousSibling.innerHTML = "left";
+        this.nextSibling.innerHTML = "right";
     } else{
         this.classList.remove("filled");
         this.classList.add("empty");    
     }
+    */
     //this.setAttribute("style", "background:#ebebeb;");
-}
