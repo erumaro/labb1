45ee46579974f7916b90b107c4b75f0372e666c0
Tobias Årud <me@tobiasarud.se>
2016-03-16 22:04:55 +0100

    if monstrosity

--- labb1.js
+++ labb1.js
@@ -25,11 +25,65 @@ function makeRowsAndColumns(){
 makeRowsAndColumns();
 
 function colorChange(){
-    table.rows.classList.remove("empty");
-    table.rows.classList.add("filled");
+    var el = this;
+    if(el.classList.contains("empty") && el.previousElementSibling.classList.contains("empty") && el.nextElementSibling.classList.contains("empty")){
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("empty");
+            el.previousElementSibling.classList.add("filled");
+        }
+        el.classList.remove("empty");
+        el.classList.add("filled");
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("empty");
+            el.nextElementSibling.classList.add("filled");
+        }
+    } else if(el.classList.contains("filled") && el.previousElementSibling.classList.contains("filled") && el.nextElementSibling.classList.contains("filled")){
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("filled");
+            el.previousElementSibling.classList.add("empty");
+        }
+        el.classList.remove("filled");
+        el.classList.add("empty");
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("filled");
+            el.nextElementSibling.classList.add("empty");
+        } 
+    } else if(el.classList.contains("empty") && el.previousElementSibling.classList.contains("filled") && el.nextElementSibling.classList.contains("filled")){
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("filled");
+            el.previousElementSibling.classList.add("empty");
+        }
+        el.classList.remove("empty");
+        el.classList.add("filled"); 
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("filled");
+            el.nextElementSibling.classList.add("empty");
+        }
+    } else{
+        if(el.previousElementSibling != null){
+            el.previousElementSibling.classList.remove("empty");
+            el.previousElementSibling.classList.add("filled");
+        }
+        el.classList.remove("filled");
+        el.classList.add("empty"); 
+        if(el.nextElementSibling != null){
+            el.nextElementSibling.classList.remove("empty");
+            el.nextElementSibling.classList.add("filled");
+        }
+    }
 }
 
 /*
+    for(var i = 0; i < 5; i++){
+        var x = table.rows[i].cells;
+        for(var j = 0; j < 5; j++){
+        x[j].classList.remove("empty");
+        x[j].classList.add("filled");      
+        }     
+    }
+
+
+
 function changeContent(id, row, cell, content) {
     var x = document.getElementById(id).rows[row].cells;
     x[cell].innerHTML = content;
